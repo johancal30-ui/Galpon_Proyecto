d9dc98e5e362ff0457fbad718138fe933f6f6083
Johan Castillo <johancal30@gmail.com>
2026-09-21 11:13:01 -0500

    Implemento modelo de RegistroProduccion

--- Galpon_Proyecto/models.py
+++ Galpon_Proyecto/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class RegistroProduccion(models.Model):
+    fecha = models.DateField(auto_now_add=True)  # Guarda la fecha automáticamente
+    huevos_recolectados = models.IntegerField()
+    cubetas_armadas = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.fecha} - {self.huevos_recolectados} huevos"
